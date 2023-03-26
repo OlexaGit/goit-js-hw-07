df7719306e96be49871da67527635cf5e7bedece
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-03-26 22:02:46 +0300

    HW-7 task-01 esc_process

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+// =================
 const galleryContainer = document.querySelector("ul.gallery");
-// document.addEventListener("keydown", (event) => {
-//   console.log("Keydown: ", event.key);
-// });
-
 const addHtmlElemenys = galleryItems
   .map(({ preview, original, description }) => {
     return `<li class="gallery__item">
@@ -36,3 +32,64 @@ galleryContainer.onclick = (event) => {
     .show();
   console.log(event.target.dataset.source);
 };
+
+// ============
+// const galleryContainer = document.querySelector("ul.gallery");
+// document.addEventListener("keydown", (event) => {
+//   // console.log("Keydown: ", event.key);
+// });
+// const handleClick = (event) => {
+//   console.log("click event listener callback");
+//   if (!event.target.classList.contains("gallery__image")) {
+//     console.log("ERROR");
+//     return;
+//   }
+//   event.preventDefault();
+//   console.log(event.target.dataset.source);
+// };
+// const runEsc = (evt) => {
+//   console.log("listener Esc");
+//   // instance.close();
+//   console.log(handleClick(evt));
+// };
+
+// const addHtmlElemenys = galleryItems
+//   .map(({ preview, original, description }) => {
+//     return `<li class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"/>
+//   </a>
+// </li>`;
+//   })
+//   .join("");
+
+// galleryContainer.insertAdjacentHTML("beforeend", addHtmlElemenys);
+
+// galleryContainer.addEventListener("click", handleClick);
+// galleryContainer.addEventListener("keydown", runEsc);
+
+// // const instance = basicLightbox.create(
+// //   `
+// // 		<img width="1400" height="900" src="${event.target.dataset.source}">
+// // 	`
+// // );
+
+// galleryContainer.onclick = (event) => {
+//   const instance = basicLightbox.create(
+//     `
+//   	<img width="1400" height="900" src="${event.target.dataset.source}">
+//   `
+//   );
+//   instance.show();
+//   // console.log(event.target.dataset.source);
+// };
+
+// // instance.close();
+// // galleryContainer.onkeydown = (event) => {
+// //   basicLightbox.create().close();
+// //   console.log(event.target.dataset.source);
+// // };
